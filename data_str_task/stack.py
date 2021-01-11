29e6f4002a37944a190fae68d8e7c8b27b740c7a
marianasargsyan <mariana.sargsyann@gmail.com>
2021-01-11 17:29:27 +0400

    data_structure_task

---
+++ data_str_task/stack.py
@@ -0,0 +1,61 @@
+# Create stack with the first in first out principle
+
+class Stack:
+    def __init__(self, input):
+        self.list = []
+
+    def push(self, i):
+        self.input = self.input.append(i)
+        return self.input
+
+    def popnew(data):
+        datanew = data[-len(data)]
+        return datanew
+
+
+
+
+
+x = range(1,100)
+
+def popnew(data):
+    datanew = data[-len(data)]
+
+    return datanew
+
+
+class Stack:
+    def __init__(self, initial_size=100):
+        self.lst = [] * initial_size
+        self.index = -1
+
+    def push(self, data):
+        self.index += 1
+        self.lst[self.index] = data
+
+    def pop(self):
+        newdata = remove(data[-len(data)])
+        return newdata
+
+    def checklast(self):
+        print(newdata[-1])
+
+    def size(self):
+        return len(self.data)
+
+
+
+
+
+class Stack:
+
+    def __init__(self, init_size = 100):
+        self.lst = [] * init_size
+        self.index = -1
+
+    def push(self, data):
+        self.index += 1
+        data = self.lst[self.index]
+
+
+
