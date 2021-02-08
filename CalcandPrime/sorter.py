be8fb18ad8e7910d4843b2e72e0d4b05b446348c
zh_123 <zhora.gevorgyan@makeinarmenia.com>
2021-02-08 22:23:08 +0400

    Sorter minor cleanup.

--- CalcandPrime/sorter.py
+++ CalcandPrime/sorter.py
@@ -5,15 +5,15 @@ def sorter(lst):
     arr = [0]*(max(lst) + 1)
     for number in lst:
         arr[number] += 1
+    out_string = ''
     for i in range(len(arr)):
         if arr[i] != 0:
-            print(str(i)*arr[i])
-
+            out_string += (str(i) + ', ') * arr[i]
+    print(out_string)
 
 
 if __name__ == '__main__':
     lst1 = []
     for _ in range(0, 10):
-        lst1.append(randint(0, 100))
-
-    print(sorter(lst1))
+        lst1.append(randint(0, 50))
+    sorter(lst1)
